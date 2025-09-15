78ff787dc28b9e7bc6538cd01bb9f24819b1a746
starianna <159666902+starianna@users.noreply.github.com>
2025-09-15 18:08:45 -0230

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 console.log("Starting process...");
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from my app!');
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/frontend/templates', (req, res) => {
+  res.json({ message: 'This is some game data.' });
 });
 
 module.exports = app;
